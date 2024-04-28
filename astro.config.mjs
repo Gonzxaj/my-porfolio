bcb2c35dc650d95fac72fa692289bb30ef5e69f7
Gonzxa <g.carranza@letscode.ar>
2024-04-28 02:55:42 -0300

    optimize

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,22 @@ import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
+import playformCompress from "@playform/compress";
+import robotsTxt from "astro-robots-txt";
+import sitemap from "@astrojs/sitemap";
+import compressor from "astro-compressor";
+
+import purgecss from "astro-purgecss";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    icon(),
-  ],
+  integrations: [tailwind(), icon(), playformCompress(), robotsTxt({
+    // host : 'https://gonzalo-carranza.vercel.app',
+  }), sitemap(), compressor({
+    gzip: true,
+    brotli: false
+  }), purgecss()],
   output: 'server',
   adapter: vercel(),
+  site: 'https://gonzalo-carranza.vercel.app'
 });
