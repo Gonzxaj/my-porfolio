b4d757f4cbbe9118d298a02ac0e9d8ca19f35018
Gonzalo Carranza <gonzalo.carranza2001@gmail.com>
2026-03-22 17:14:09 -0300

    refactor: remove unused sitemap and robotsTxt integrations from astro.config.mjs; clean up robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,18 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import playformCompress from "@playform/compress";
-// import robotsTxt from "astro-robots-txt";
-import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: 'https://gonzalo-carranza.pages.dev',
-  trailingSlash: 'always',
   integrations: [
     tailwind(),
     icon(),
     playformCompress(),
-    sitemap(),
-    // robotsTxt(),
   ],
   output: 'static',
   i18n: {
